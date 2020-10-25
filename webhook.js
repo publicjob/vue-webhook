bbd4453ce8cc4e164bdb68ab120c8628adef1f9f
work.cjl <work.cjl@hotmail.com>
2020-10-25 18:07:11 +0800

    feat: webhook

---
+++ webhook.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+
+const sever = http.createServer(function(req, res) {
+  console.log(req.method, req.url) 
+  if(req.method == 'POST' && req.url =='/webhook'){
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ok: true}))
+  }else {
+    res.end('not found!')
+  }
+})
+
+server.listen(4000, function() {
+  console.log('服务器启动在4000端口上！')
+})
